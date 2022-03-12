55b600e109ac15db8978f26b37880cfdec36b38b
KJohnson-prof <k.johnson3271@gmail.com>
2022-03-12 12:38:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/steal_tastic/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.steal_tastic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/** database & login/sign up activities
+ * -tell us api keys
+ * -mandatory follow categories
+ * main activity
+ * -feed of categories they follow
+ * -task bar for navigation
+ *post activity
+ *
+ *search activity
+ * **/
