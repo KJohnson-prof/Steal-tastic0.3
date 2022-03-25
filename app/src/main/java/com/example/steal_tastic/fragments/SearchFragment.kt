61eb0ad42580ffd122a8cb590284ccb6d16fc73e
nyang5504 <ninayang3.14@gmail.com>
2022-03-25 14:32:30 -0400

    Search Fragment edited

--- app/src/main/java/com/example/steal_tastic/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/steal_tastic/fragments/SearchFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.steal_tastic.Post
 import com.example.steal_tastic.PostAdapter
@@ -18,6 +21,7 @@ import com.parse.ParseQuery
 open class SearchFragment : Fragment() {
 
     lateinit var adapter: PostAdapter
+    lateinit var  SearchRecyclerView: RecyclerView
     var allPosts: MutableList<Post> = mutableListOf()
     lateinit var swipeContainer: SwipeRefreshLayout
 
@@ -33,12 +37,41 @@ open class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.btnSearch).setOnClickListener {
-
+            val searchFor = view.findViewById<EditText>(R.id.tvSearch).text.toString()
+            queryPosts(searchFor)
         }
+
+        SearchRecyclerView = view.findViewById(R.id.SearchRecyclerView)
+
+        //swipeContainer = view.findViewById(R.id.swipeContainer)
+
+//        swipeContainer.setOnRefreshListener {
+//            Log.i(TAG, "Refreshing timeline")
+//            queryPosts()
+//        }
+//
+//        // Configure the refreshing colors
+//        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+//            android.R.color.holo_green_light,
+//            android.R.color.holo_orange_light,
+//            android.R.color.holo_red_light)
+
+        //Steps to populate RecyclerView
+        //1. Create layout for each row in list (item_post.xml)
+        //2. Create data source for each row(this is the post class)
+        //3. Create adapter that will bridge data and row layout (PostAdapter)
+        //4. Set adapter on RecyclerView
+        adapter = PostAdapter(requireContext(), allPosts as ArrayList<Post>)
+        SearchRecyclerView.adapter = adapter
+        //5. Set layout manager on RecyclerView
+
+        SearchRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //queryPosts()
 
     }
 
-    open fun queryPosts(){
+    open fun queryPosts(string: String){
 
         //specify which class to query
         val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
@@ -54,10 +87,16 @@ open class SearchFragment : Fragment() {
                     adapter.clear()
                     if(posts != null) {
                         for (post in posts) {
-                            Log.i(TAG, "Post: " + post.getDescription() + " , username: " + post.getUser()?.username)
+                            Log.i(TAG, "Post: " + post.getDescription() )
                         }
 
-                        allPosts.addAll(posts)
+                        for(item in posts){
+                            if(){ //if post has a tag of search value the add to list.
+                                allPosts.add(item)
+                            }
+
+                        }
+
 
                         //allPosts.addAll(posts)
                         adapter.notifyDataSetChanged()
