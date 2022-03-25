2dc0c9c370f588a63d2d85e940326e640646b6db
nyang5504 <ninayang3.14@gmail.com>
2022-03-25 19:09:00 -0400

    Navigation Bar and Show fragments

--- app/src/main/java/com/example/steal_tastic/MainActivity.kt
+++ app/src/main/java/com/example/steal_tastic/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.steal_tastic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.steal_tastic.fragments.ComposeFragment
+import com.example.steal_tastic.fragments.FeedFragment
+import com.example.steal_tastic.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+                item ->
+
+            var fragmentToShow: Fragment? = null
+            when(item.itemId){
+                R.id.action_home -> {
+                    //Navigate to the home screen / feed fragment
+                    fragmentToShow = FeedFragment()
+                }
+                R.id.action_compose -> {
+                    //Navigate to the compose screen
+                    fragmentToShow = ComposeFragment()
+                }
+                R.id.action_search -> {
+                    fragmentToShow = SearchFragment()
+                }
+            }
+            if(fragmentToShow != null){
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+            // Return true to say that we've handled this user interaction on the item
+            true
+        }
+
+        // Set default selection
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_home
     }
 }
 
